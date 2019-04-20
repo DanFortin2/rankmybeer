cc31f4427a8119c7c3067a6f7123f7030001fd2d
unknown <fortin_85@hotmail.com>
2019-04-20 11:34:20 -0400

    rebuild website

---
+++ precache-manifest.9d0d0bcbca6120c68a7053aac56ecc8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b883a689b603c0365292cf8041db70f2",
+    "url": "/DanFortin2/rankmybeer/static/media/brewery.b883a689.jpg"
+  },
+  {
+    "revision": "fc558ddebce7251f3f5e",
+    "url": "/DanFortin2/rankmybeer/static/js/runtime~main.d75f4152.js"
+  },
+  {
+    "revision": "45eaffb7e177858992a8",
+    "url": "/DanFortin2/rankmybeer/static/js/main.e4d3e64f.chunk.js"
+  },
+  {
+    "revision": "d53b0bcea785b04c5888",
+    "url": "/DanFortin2/rankmybeer/static/js/2.664d9300.chunk.js"
+  },
+  {
+    "revision": "45eaffb7e177858992a8",
+    "url": "/DanFortin2/rankmybeer/static/css/main.64d21b46.chunk.css"
+  },
+  {
+    "revision": "4eb321e365f8570770d254fd27538bde",
+    "url": "/DanFortin2/rankmybeer/index.html"
+  }
+];
